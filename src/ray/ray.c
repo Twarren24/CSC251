ca42610038ab9b2d31bad86fec6eb3b1814cb936
Twarren24 <158978381+Twarren24@users.noreply.github.com>
2024-05-06 21:42:41 -0700

    Update ray.c

--- src/ray/ray.c
+++ src/ray/ray.c
@@ -6,11 +6,11 @@ Ray ray(Tuple origin, Tuple direction) {
 }
    
 Tuple position(Ray r, double t) {
-    // Calculate the position along the ray using the parametric equation
+   
     double x = r.origin.x + r.direction.x * t;
     double y = r.origin.y + r.direction.y * t;
     double z = r.origin.z + r.direction.z * t;
-    return (Tuple){x, y, z, 1}; // Assuming a homogeneous coordinate of 1 for points3
+    return (Tuple){x, y, z, 1}; 
 }
 
 Sphere sphere(Tuple center, double radius) {
